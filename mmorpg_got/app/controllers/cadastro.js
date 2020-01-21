c4681eab5e78588a62a387ed9cf023f70ad1a217
lucas_petrolli <lucas.almeida@somoskratos.com.br>
2020-01-21 12:52:10 -0400

    Adiciona validação do formulario pra criar usuario

--- mmorpg_got/app/controllers/cadastro.js
+++ mmorpg_got/app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = (application, req, res) => {
-    res.render('cadastro')
+    res.render('cadastro', { validacao: {}, dadosForm: {} })
+}
+
+module.exports.cadastrar = (application, req, res) => {
+    let dadosForm = req.body;
+
+    req.assert('nome', 'O campo nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'O campo usuario não pode ser vazio').notEmpty();
+    req.assert('senha', 'O campo senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa é obrigatorio').notEmpty();
+
+    let erros = req.validationErrors();
+
+    if(erros) {
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+        return;
+    }
+
+    res.send('Podemos cadastrar');
 }
